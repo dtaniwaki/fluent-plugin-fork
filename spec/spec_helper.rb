34ba7487d1fe5bd3f5907b9dc18534560a305a35
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-08-18 16:20:35 +0900

    Migrate to use v0.14 API

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require 'coveralls'
 Coveralls.wear!
 
 require 'fluent/test'
+require 'fluent/test/driver/output'
+require 'fluent/test/helpers'
 require 'fluent/plugin/out_fork'
 
 RSpec.configure do |config|
